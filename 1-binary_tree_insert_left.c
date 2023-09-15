d34b1b3d3e9004ae5b97651743905b09327cbf11
alex <6267@holbertonstudents.com>
2023-09-15 05:06:36 -0700

    Write a function that checks if a node is a leaf

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,7 +1,10 @@
 #include "binary_trees.h"
 /**
+ * binary_tree_insert_left - Inserts a node as the left child of a parent node
+ * @parent: Pointer to the parent node
+ * @value: Value to store in the new node
  *
- *
+ * Return: Pointer to the newly created node, or NULL on failure
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,8 +1,12 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_insert_right - Inserts a node as the right child
+ * of a parent node
+ * @parent: Pointer to the parent node
+ * @value: Value to store in the new node
  *
- *
+ * Return: Pointer to the newly created node, or NULL on failure
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
